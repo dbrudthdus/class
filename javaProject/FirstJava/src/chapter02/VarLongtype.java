fa309e98730a45d1468d02333761307e0900209f
dbrudthdus <dbrudthdus@gmail.com>
2023-03-22 17:32:45 +0900

    03.21 수업

---
+++ javaProject/FirstJava/src/chapter02/VarLongtype.java
@@ -0,0 +1,17 @@
+package chapter02;
+
+public class VarLongtype {
+
+	public static void main(String[] args) {
+	
+		
+		long num = 10000000000L;
+		
+		System.out.println(num);
+		
+		// byte num2 = 300;
+		
+		
+	}
+
+}
